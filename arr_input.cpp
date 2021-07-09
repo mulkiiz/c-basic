71dc4fa40cecb22858a70c4ed3033e3bc62654e4
mulkiiz <admiportfolio@gmail.com>
2021-07-09 11:36:22 +0700

    update arr_input.cpp

--- arr_input.cpp
+++ arr_input.cpp
@@ -6,12 +6,12 @@ int main ()
   int i; 
   /* baca isi Tab */
   for (i=0; i<5; i++) {
-     scanf ("%d", &Tab[i]);
+     scanf ("%d", &Tab[i]); //pasangan dari scanf, pasti &{nama_var}
   }  
  
   /* menuliskan isi Tab berderet ke kanan*/
   for (i=0; i<5; i++) {
-     printf ("Tab[%d]= %d ;", i,  Tab[i]);
+     printf ("Tab[%d]= %d ;", i,  Tab[i]); //cetak nilai var sebanding dengan nilai %d atau %{scope_typedata}
   }
   printf ("\n");
   
